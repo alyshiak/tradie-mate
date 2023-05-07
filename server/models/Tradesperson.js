4628ca15e260fd882b31d857de86e97cfdc37768
Alyshia Kandler <alyshiakandler@Alyshias-MacBook-Air.local>
2023-05-07 17:48:42 +1000

    models started

---
+++ server/models/Tradesperson.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require('mongoose');
+const mongoose = require("mongoose");
+require('mongoose-type-email');
+
+const favouriteSchema = new Schema({
+  favouriteId: {
+    type: mongoose.ObjectId,
+  },
+  favourite: {
+    type: Boolean,
+  },
+});
+
+const tradespersonSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      max_length: 50,
+      min_length: 1,
+    },
+    trade: {
+      type: String,
+      required: true,
+    },
+    location: {
+    type: String,
+     required: true,
+    },
+    phone: {
+    type: String,
+    required: true,
+    },
+    email: {
+    type: mongoose.SchemaTypes.Email,
+    required: true,
+    },
+
+    favourite: [favouriteSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Tradesperson = model('tradesperson', tradespersonSchema);
+
+tradespersonSchema.virtual('favouriteCount').get(function() {
+  return this.favourite.length;
+});
+
+module.exports = Tradesperson;
