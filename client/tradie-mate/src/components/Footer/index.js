515264de20161da09b5ced5d3c46856e5d610493
Boulter97 <118101244+Boulter97@users.noreply.github.com>
2023-05-09 20:10:01 +1000

    added temp logo and component files

---
+++ client/tradie-mate/src/components/Footer/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+
---
+++ client/tradie-mate/src/components/pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+import ProfileList from '../components/ProfileList';
+
+import { QUERY_PROFILES } from '../utils/queries';
+
+
